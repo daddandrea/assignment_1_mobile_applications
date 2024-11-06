5ace0f3e074d284479e787c711962c98ac742fc9
daddandrea <andrea.daddabbo@gmail.com>
2024-11-06 17:20:38 +0100

    Auto-formatted LoginActivity.kt

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -8,12 +8,11 @@ import android.widget.TextView
 class LoginActivity : AppCompatActivity() {
 
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        val registerLink : TextView = findViewById(R.id.register_now_link)
+        val registerLink: TextView = findViewById(R.id.register_now_link)
         registerLink.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
             startActivity(intent)
