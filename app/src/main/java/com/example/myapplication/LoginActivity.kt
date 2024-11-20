f0c774ff20bec514b95848a40b96174a6f6de075
daddandrea <andrea.daddabbo@gmail.com>
2024-11-20 17:45:31 +0100

    Incorporated login page authentication based on CredentialsManager functions

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -2,15 +2,36 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val emailInputLayout: TextInputLayout = findViewById(R.id.login_email_input_layout)
+        val emailInput: TextInputEditText = findViewById(R.id.login_email_input)
+
+        val passwordInputLayout: TextInputLayout =
+            findViewById(R.id.register_password_input_layout)
+        val passwordInput: TextInputEditText = findViewById(R.id.register_password_input)
+
+        val loginButton: Button = findViewById(R.id.login_next_button)
+
+        val credentialsManager = CredentialsManager()
+
+        super.onCreate(savedInstanceState)
+
+        loginButton.setOnClickListener {
+            if (!credentialsManager.login(emailInput.text.toString(), passwordInput.text.toString())) {
+                emailInputLayout.error = "Please insert a valid email."
+                passwordInputLayout.error = "Please insert a valid password"
+            }
+        }
 
         val registerLink: TextView = findViewById(R.id.register_now_link)
         registerLink.setOnClickListener {
