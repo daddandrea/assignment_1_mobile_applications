2d4f430d4c44ca872ccd1a7321f10cff2d228817
daddandrea <andrea.daddabbo@gmail.com>
2024-11-25 20:34:44 +0100

    Refactored RegisterActivity for more granular control on player input validation

--- app/src/main/java/com/example/myapplication/RegisterActivity.kt
+++ app/src/main/java/com/example/myapplication/RegisterActivity.kt
@@ -14,10 +14,12 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val fullNameInputLayout: TextInputLayout = findViewById(R.id.register_full_name_input_layout)
+        val fullNameInputLayout: TextInputLayout =
+            findViewById(R.id.register_full_name_input_layout)
         var fullNameInput: TextInputEditText
 
-        val phoneNumberInputLayout: TextInputLayout = findViewById(R.id.register_phone_number_input_layout)
+        val phoneNumberInputLayout: TextInputLayout =
+            findViewById(R.id.register_phone_number_input_layout)
         var phoneNumberInput: TextInputEditText
 
         val emailInputLayout: TextInputLayout = findViewById(R.id.register_valid_email_input_layout)
@@ -35,17 +37,14 @@ class RegisterActivity : AppCompatActivity() {
             emailInput = findViewById(R.id.register_valid_email_input)
             passwordInput = findViewById(R.id.register_password_input)
 
-            if (credentialsManager.register(fullNameInput.text.toString(), phoneNumberInput.text.toString(), emailInput.text.toString(), passwordInput.text.toString())) {
-                fullNameInputLayout.error = "Please enter a name."
-                phoneNumberInputLayout.error = "Please enter a phone number."
-                emailInputLayout.error = "Please enter an email address."
-                passwordInputLayout.error = "Please enter a password."
-            } else {
-                fullNameInputLayout.error = null
-                phoneNumberInputLayout.error = null
-                emailInputLayout.error = null
-                passwordInputLayout.error = null
-            }
+            fullNameInputLayout.error =
+                if (!credentialsManager.isFullNameValid(fullNameInput.text.toString())) "Please insert a full name" else null
+            phoneNumberInputLayout.error =
+                if (!credentialsManager.isPhoneNumberValid(phoneNumberInput.text.toString())) "Please insert a phone number" else null
+            emailInputLayout.error =
+                if (!credentialsManager.isEmailValid(emailInput.text.toString())) "Please insert a valid email" else null
+            passwordInputLayout.error =
+                if (!credentialsManager.isPasswordValid(passwordInput.text.toString())) "Please insert a valid password" else null
         }
 
         val loginLink: TextView = findViewById(R.id.login_now_link)
