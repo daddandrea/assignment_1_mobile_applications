3a29f75a45c319e52c2e31a19b8dd092f3af09d4
daddandrea <andrea.daddabbo@gmail.com>
2024-12-13 21:06:16 +0100

    Updated logic for login() method and added a method that logs credentials.

--- app/src/main/java/com/example/myapplication/CredentialsManager.kt
+++ app/src/main/java/com/example/myapplication/CredentialsManager.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication
 
+import android.util.Log
+
 private const val DUMMY_EMAIL = "test@te.st"
 private const val DUMMY_PASSWORD = "1234"
 
@@ -12,12 +14,18 @@ class CredentialsManager {
             "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
             ")+")
 
-    private val credentials = hashMapOf<String, String>(Pair(DUMMY_EMAIL, DUMMY_PASSWORD))
+    private val credentials = hashMapOf(Pair(DUMMY_EMAIL, DUMMY_PASSWORD))
 
     fun clearCredentials() {
-        val initialCredentials = hashMapOf<String, String>(Pair(DUMMY_EMAIL, DUMMY_PASSWORD))
+        val initialCredentials = hashMapOf(Pair(DUMMY_EMAIL, DUMMY_PASSWORD))
         credentials.keys.retainAll(initialCredentials.keys)
         credentials[DUMMY_EMAIL] = DUMMY_PASSWORD
+    }
+
+    fun logCredentials() {
+        for ((key, value) in credentials) {
+            Log.d("Debug", "$key --> $value")
+        }
     }
 
     fun isEmailValid(email: String): Boolean {
@@ -37,7 +45,7 @@ class CredentialsManager {
     }
 
     fun login(email: String, password: String): Boolean {
-        return credentials.containsKey(email.lowercase()) && password == credentials[email]
+        return credentials[email.lowercase()] == password
     }
 
     fun register(fullName: String, phoneNumber: String, email: String, password: String): Boolean {
