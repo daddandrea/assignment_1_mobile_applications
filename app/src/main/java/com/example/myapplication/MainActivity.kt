5702033a80892beac2622eeac8381cc43404f32b
daddandrea <andrea.daddabbo@gmail.com>
2025-01-15 15:31:46 +0100

    Added search functionality to the MainActivity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,45 +1,18 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.SearchView
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    private val recipes = listOf(
-        Recipe(
-            1,
-            "Spaghetti alla carbonara",
-            "Gli spaghetti migliori al mondo",
-            R.drawable.carbonara,
-            arrayListOf("Spaghetti", "Guanciale", "Pecorino", "Pepe nero", "Uova")
-        ),
-        Recipe(
-            2,
-            "Spaghetti all'assassina",
-            "Gli spaghetti di Bari migliori al mondo",
-            R.drawable.assassina,
-            arrayListOf("Spaghetti", "Salsa di pomodoro", "Peperoncino", "Stracciatella")
-        ),
-        Recipe(
-            3,
-            "Pizza margherita",
-            "La pizza migliore al mondo",
-            R.drawable.margherita,
-            arrayListOf(
-                "Farina",
-                "Acqua",
-                "Lievito di birra",
-                "Olio extravergine d'oliva",
-                "Sale fino",
-                "Pomodori pelati",
-                "Grana padano DOP",
-                "Basilico",
-                "Mozzarella"
-            )
-        )
-    )
+    private val recipeViewModel: RecipeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,7 +22,7 @@ class MainActivity : AppCompatActivity() {
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         val adapter = RecipeAdapter(
-            recipes = recipes,
+            recipes = emptyList(),
             onItemClick = { recipe ->
                 Toast.makeText(this, "Clicked on: ${recipe.name}", Toast.LENGTH_SHORT).show()
             },
@@ -58,5 +31,24 @@ class MainActivity : AppCompatActivity() {
             }
         )
         recyclerView.adapter = adapter
+
+        lifecycleScope.launch {
+            recipeViewModel.recipes.collectLatest { updatedRecipes ->
+                adapter.updateRecipes(updatedRecipes)
+            }
+        }
+
+        val searchView: SearchView = findViewById(R.id.search_view)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                recipeViewModel.queryRecipes(newText.orEmpty())
+                return true
+            }
+
+        })
     }
 }
